bd7cfad0aec690e12c8540d124bf9597a323266d
vanessanader <vanessanader1@hotmail.com>
2018-04-23 23:17:26 +0300

    Did about us and help

--- aPollo/AboutUsViewController.swift
+++ aPollo/AboutUsViewController.swift
@@ -1,20 +1,22 @@
 //
-//  HelpViewController.swift
+//  AboutUsViewController.swift
 //  aPollo
 //
-//  Created by Vanessa Nader on 4/10/18.
+//  Created by Vanessa Nader on 4/23/18.
 //  Copyright © 2018 Vanessa Nader. All rights reserved.
 //
 
 import UIKit
 
-class HelpViewController: UIViewController {
+class AboutUsViewController: UIViewController {
 
-    @IBOutlet weak var menuBarButton: UIBarButtonItem!
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    @IBOutlet weak var text: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        menuBarButton.target = self.revealViewController()
-        menuBarButton.action = #selector(SWRevealViewController.revealToggle(_:))
+        menuButton.target = self.revealViewController()
+        menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+        
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         // Do any additional setup after loading the view.
     }
@@ -22,7 +24,6 @@ class HelpViewController: UIViewController {
     override var prefersStatusBarHidden: Bool {
         return true
     }
-    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
